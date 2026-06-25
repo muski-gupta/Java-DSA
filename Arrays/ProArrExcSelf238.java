ed0efbae1b35cd55a1a4616574453e18f6044c70
Muskan_gupta <musku.2200@gmail.com>
2026-06-25 10:11:52 +0530

    fourth q

--- Arrays/ProArrExcSelf238.java
+++ Arrays/ProArrExcSelf238.java
@@ -1,22 +1,54 @@
-
-
-public class ProArrExcSelf238 {
-    public static void main(String[] args){
-     int nums[]={1,2,3,4};
-     MUL(nums);
+//optimized code
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        return product(nums);
     }
-    public static void MUL(int nums[]){
-     int n=nums.length;
-     int []left =new int[n];
-     left[0]=1;
-     for(int i=1;i<n;i++){
-      left[i]=left[i-1]*nums[i-1];
-     }
-     int [] right=new int[n];
-     right[n-1]=1;
-     for(int i=n-2;i>=0;i--){
-      right[i]=right[i+1]*nums[i+1];
-     }
-
+      public static int[] product(int []nums){
+        int n=nums.length;
+        int []left=new int[n];
+        left[0]=1;
+        for(int i=1;i<n;i++){
+            left[i]=left[i-1]*nums[i-1];
+        }
+        int []right=new int[n];
+        right[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            right[i]=right[i+1]*nums[i+1];
+        }
+        for(int i=0;i<n;i++){
+            left[i]=left[i]*right[i];
+        }
+        return left;
+      }  
     }
-  }
+
+//non optimized code 
+
+// public class ProArrExcSelf238 {
+//     public static void main(String[] args){
+//      int nums[]={1,2,3,4};
+//     int ans[]=product(nums);
+//     for(int i=0;i<ans.length;i++){
+//       System.out.print(ans[i] + " ");
+//     }
+//     }
+//     public static int[] product(int[]nums){
+//      int n=nums.length;
+//      int []left =new int[n];
+//      left[0]=1;
+//      for(int i=1;i<n;i++){
+//       left[i]=left[i-1]*nums[i-1];
+//      }
+//      int [] right=new int[n];
+//      right[n-1]=1;
+//      for(int i=n-2;i>=0;i--){
+//       right[i]=right[i+1]*nums[i+1];
+//      }
+     
+
+//      for(int i=0;i<n;i++){
+//       left[i]=left[i]*right[i];
+//      }
+//      return left;
+//     }
+//   }
