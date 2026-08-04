a7b6056fd547de473de6180e4c9e2f16a8e8571d
muski-gupta <musku.2200@gmail.com>
2026-08-04 20:25:16 +0530

    another method to count

--- Recurrsion/SubSeq.java
+++ Recurrsion/SubSeq.java
@@ -17,21 +17,39 @@ public class SubSeq {
 
 
 //how to count the no. of possible sub sequences 
-    public static void main(String [] args){
-        String ques="abc";
-        PrintSubSeq(ques," ");
-        System.out.println(count);
-    }
-     static int count = 0;
-    public static void PrintSubSeq(String ques, String ans) {
-        if(ques.length()==0){
-            System.out.print(ans + " ");
-            count++;
-            return ;
-        }
-        char ch=ques.charAt(0);
-        PrintSubSeq(ques.substring(1), ans);
-        PrintSubSeq(ques.substring(1), ans+ch);
-    }
 
+//method 1
+    // public static void main(String [] args){
+    //     String ques="abc";
+    //     PrintSubSeq(ques," ");
+    //     System.out.println(count);
+    // }
+    //  static int count = 0;
+    // public static void PrintSubSeq(String ques, String ans) {
+    //     if(ques.length()==0){
+    //         System.out.print(ans + " ");
+    //         count++;
+    //         return ;
+    //     }
+    //     char ch=ques.charAt(0);
+    //     PrintSubSeq(ques.substring(1), ans);
+    //     PrintSubSeq(ques.substring(1), ans+ch);
+    // }
+
+
+//method 2 
+public static void main(String [] args){
+    String ques="abc";
+    System.out.print(PrintSubSeq(ques," "));
+}
+public static int PrintSubSeq(String ques,String ans) {
+    if(ques.length()==0){
+        // System.out.print(ans + " ");
+        return 1;
+    }
+    char ch=ques.charAt(0);
+   int a1= PrintSubSeq(ques.substring(1), ans);
+   int b1= PrintSubSeq(ques.substring(1), ans+ch);
+   return a1+b1;
+}
 }
