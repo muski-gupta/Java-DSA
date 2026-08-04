5b52975937b28be95dde92de4beade5706421852
muski-gupta <musku.2200@gmail.com>
2026-08-04 22:54:35 +0530

    Toss a coin

---
+++ Recurrsion/CoinToss.java
@@ -0,0 +1,17 @@
+public class CoinToss {
+
+    //print the number of possibilties while tossing a coin and counting the number also 
+    public static void main(String [] args){
+        int n=2;
+        System.out.print(TossCoin(n," "));
+    }
+    public static int TossCoin(int n,String ans) {
+        if(n==0){
+            System.out.print(ans + " ");
+            return 1;
+        }
+       int a=TossCoin(n-1, ans+ "H");
+       int b=TossCoin(n-1, ans+ "T");
+       return a+b;
+    }
+}
