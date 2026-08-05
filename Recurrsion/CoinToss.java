f5b3ea860a256c56eccb554aeee476678a618ab2
muski-gupta <musku.2200@gmail.com>
2026-08-05 20:25:09 +0530

    reduce consecutive to heads

--- Recurrsion/CoinToss.java
+++ Recurrsion/CoinToss.java
@@ -1,17 +1,33 @@
 public class CoinToss {
 
     //print the number of possibilties while tossing a coin and counting the number also 
-    public static void main(String [] args){
-        int n=2;
-        System.out.print(TossCoin(n," "));
+    // public static void main(String [] args){
+    //     int n=3;
+    //     System.out.print(TossCoin(n," "));
+    // }
+    // public static int TossCoin(int n,String ans) {
+    //     if(n==0){
+    //         System.out.print(ans + " ");
+    //         return 1;
+    //     }
+    //    int a=TossCoin(n-1, ans+ "H");
+    //    int b=TossCoin(n-1, ans+ "T");
+    //    return a+b;
+    // }
+
+    //remove the possibilities where there is consecutive two heads 
+     public static void main(String [] args){
+        int n=3;
+        TossCoin(n," ");
     }
-    public static int TossCoin(int n,String ans) {
+    public static void TossCoin(int n,String ans) {
         if(n==0){
             System.out.print(ans + " ");
-            return 1;
+            return ;
         }
-       int a=TossCoin(n-1, ans+ "H");
-       int b=TossCoin(n-1, ans+ "T");
-       return a+b;
+        if(ans.length()==0 || ans.charAt(ans.length()-1) != 'H'){
+         TossCoin(n-1, ans+ "H");
+        }
+       TossCoin(n-1, ans+ "T");
     }
 }
