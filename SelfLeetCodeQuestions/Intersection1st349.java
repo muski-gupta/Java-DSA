f80e5f99914694d23755e4f41035b3f6208c3e23
muski-gupta <musku.2200@gmail.com>
2026-08-09 17:12:48 +0530

    leetcode 26

---
+++ SelfLeetCodeQuestions/Intersection1st349.java
@@ -0,0 +1,22 @@
+package SelfLeetCodeQuestions;
+
+public class Intersection1st349 {
+      public static void main(String [] args){
+        int [] nums1={1,2,2,1};
+        int [] nums2={2,2};
+        System.out.println(intersection(nums1, nums2));
+
+      }
+        public static int[] intersection(int[] nums1, int[] nums2) {
+        ArrayList<Integer> ans =new Arraylist <> ();
+        // List<String> ans = new ArrayList<> ();
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    ans.add(nums1[i]);
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ SelfLeetCodeQuestions/RemoveDuplicates26.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class RemoveDuplicates26 {
+    public static void main(String [] args){
+     int [] nums={1,1,2,2,3,4};
+     Duplicates(nums);
+    }
+    // public static void Duplicates(int [] nums){
+
+    // by using array 
+
+    //  int i=0;
+    //  for(int j=1;j<nums.length;j++){
+    //     if(nums[j]!=nums[i]){
+    //         i++;
+    //         nums[i]=nums[j];
+    //     }
+    //  }
+    //    System.out.println(i+1);     
+    // }
+
+
+    //by using arraylist
+public static ArrayList<Integer> Duplicates(int []nums){
+    ArrayList<Integer> list =new ArrayList <>();
+    list.add(nums[0]);
+    for(int i=1;i<nums.length;i++){
+        if(nums[i] != nums[i-1]){
+            list.add(nums[i]);
+
+        }
+    }
+    System.out.println(list);
+    System.out.println(list.size());
+}
+}
