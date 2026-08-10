0baf8fc13d1751d5395fac83bebc58561f2aa692
muski-gupta <musku.2200@gmail.com>
2026-08-10 20:42:35 +0530

    some changes arraylist

--- Recurrsion/ParanthesisPrint.java
+++ Recurrsion/ParanthesisPrint.java
@@ -1,19 +1,24 @@
+import java.util.*;
 public class ParanthesisPrint {
     public static void main(String[] args){
         int n=3;
-        Paranthesis(n,0,0," ");
+        List<String> ll=new ArrayList<> ();
+        Paranthesis(n,0,0," ",ll);
+        System.out.print(ll);
+
 
     }
-       public static void Paranthesis(int n,int open,int close,String ans){
+       public static void Paranthesis(int n,int open,int close,String ans,List<String> ll){
         if(open==n && close == n){
-            System.out.print(ans + " ");
+            // System.out.print(ans + " ");
+            ll.add(ans);
             return ;
         }
         if(open<n){
-            Paranthesis(n,open+1,close, ans + "(");
+            Paranthesis(n,open+1,close, ans + "(",ll);
         }
         if(close<open){
-            Paranthesis(n,open,close+1,ans + ")");
+            Paranthesis(n,open,close+1,ans + ")",ll);
         }
     }
 }
